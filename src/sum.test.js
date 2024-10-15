e8781b23cb0cdeee00cd9132275fa57ac7c0c072
AvadhiJ <avadhi.jain@habilelabs.io>
2024-10-15 11:04:26 +0530

    commit testcases

--- src/sum.test.js
+++ src/sum.test.js
@@ -10,6 +10,12 @@ it("When x & y is a number",()=>{
 it("When x & y is a String",()=>{
     const res = sum("4","5");
 
-    //res= 9
+    
     expect(res).toBe(9);
+});
+it("When x & y is a decimal",()=>{
+    const res = sum(5.3,5);
+
+    //res= 10.3
+    expect(res).toBe(10.3);
 });
