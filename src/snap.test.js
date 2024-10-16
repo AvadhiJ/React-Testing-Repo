2b516324ca037931e24857e358c94f395d9fcede
AvadhiJ <avadhi.jain@habilelabs.io>
2024-10-16 17:49:25 +0530

    commit testcases

---
+++ src/snap.test.js
@@ -0,0 +1,16 @@
+import renderer from 'react-test-renderer';
+import Link from './snap';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Link page="http://www.facebook.com">Facebook</Link>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Link page="http://www.instagram.com">Instagram</Link>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
