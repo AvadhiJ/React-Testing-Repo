bd3bb96b4616db8c880ea2938a957dfbe3803a03
AvadhiJ <avadhi.jain@habilelabs.io>
2024-10-23 18:53:55 +0530

    changes

--- cypress/e2e/smoketest.cy.js
+++ cypress/e2e/smoketest.cy.js
@@ -1,19 +1,32 @@
 describe('Smoke Testing', () => {
 
-    before(function(){
+    before(()=>
 
- cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
   
-    })
-    beforeEach(function(){
+    )
+    beforeEach(()=>
         it('Validate login functionality', () => {
             cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
             cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
             cy.get('.oxd-button').click()
         
           })
-}
+        
 )
+it('validate link',() =>{
+    cy.get(':nth-child(1) > .oxd-main-menu-item').click();
+            
+} )
 
+it('validate add button',() =>{
+    cy.get('.orangehrm-header-container > .oxd-button').click();
+            
+} )
+
+afterEach(() =>{
+
+    
+})
     
 })
