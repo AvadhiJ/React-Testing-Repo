af085ba8e6ee7c403e54ece0cead9fcca64b88ba
AvadhiJ <avadhi.jain@habilelabs.io>
2024-10-23 17:48:51 +0530

    changes

--- cypress/e2e/HomePage.cy.js
+++ cypress/e2e/HomePage.cy.js
@@ -1,5 +1,10 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
+  beforeEach(()=>cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'))
+
+  it('Validate login functionality', () => {
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    cy.get('.oxd-button').click()
+
   })
 })
